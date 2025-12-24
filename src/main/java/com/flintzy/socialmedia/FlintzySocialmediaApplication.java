bcd7aaee0e59a23a299e79d62767d7940e3f1fb8
Kiran <kirankumargv888@gmail.com>
2025-12-24 14:11:55 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/flintzy/socialmedia/FlintzySocialmediaApplication.java
@@ -0,0 +1,13 @@
+package com.flintzy.socialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlintzySocialmediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlintzySocialmediaApplication.class, args);
+	}
+
+}
